fc6e2bf0b8953d1f490b6b638f787eafe5ea271d
Artem <artem4rolov@gmail.com>
2023-03-29 18:52:07 +0300

    добавить редюсер по заданию конкретной точки с помощью карты или с помощью списка

--- src/components/PlaceOnMap/PlaceOnMap.jsx
+++ src/components/PlaceOnMap/PlaceOnMap.jsx
@@ -2,19 +2,26 @@ import React, { useRef } from "react";
 import { YMaps, Map, Placemark } from "@pbe/react-yandex-maps";
 
 import styles from "./PlaceOnMap.module.scss";
+import { useSelector } from "react-redux";
 
 const PlaceOnMap = ({ coordinates }) => {
   const defaultState = {
-    center: [55.751574, 37.573856],
-    zoom: 12,
+    center: [55.320255, 58.769415],
+    zoom: 6,
     controls: ["zoomControl", "fullscreenControl"],
   };
 
+  const { points } = useSelector((state) => state.points);
+
   const map = useRef();
 
+  // функция для позиционирования карты по координатам
   const myPanTo = (coordinates) => {
+    // предварительно проверяем, что координаты есть
+    // в противном случае показываем дефолтную точку из defaultState
     if (coordinates) {
       map.current.panTo(coordinates);
+      // map.current.
     }
   };
 
@@ -23,7 +30,7 @@ const PlaceOnMap = ({ coordinates }) => {
     myPanTo(coordinates);
   }, [coordinates]);
 
-  console.log(coordinates);
+  // console.log(coordinates);
 
   return (
     <YMaps>
@@ -33,21 +40,25 @@ const PlaceOnMap = ({ coordinates }) => {
         defaultState={defaultState}
         modules={["control.ZoomControl", "control.FullscreenControl"]}
       >
-        <Placemark
-          geometry={coordinates}
-          properties={{
-            balloonContentBody:
-              "This is balloon loaded by the Yandex.Maps API module system",
-          }}
-        />
+        {points &&
+          points.map((item) => {
+            const { address, latitude, longitude } = item;
+
+            return (
+              <Placemark
+                key={address}
+                geometry={[latitude, longitude]}
+                properties={{
+                  balloonContentBody:
+                    "This is balloon loaded by the Yandex.Maps API module system",
+                }}
+                onClick={() => {
+                  console.log([latitude, longitude]);
+                }}
+              />
+            );
+          })}
       </Map>
-      {/* <div className="myMenu">
-        <ul>
-          <li onClick={() => myPanTo([55.612392, 37.029242])}>Пункт 1</li>
-          <li onClick={() => myPanTo([55.46989, 37.947974])}>Пункт 2</li>
-          <li onClick={() => myPanTo([56.108996, 37.501654])}>Пункт 3</li>
-        </ul>
-      </div> */}
     </YMaps>
   );
 };
