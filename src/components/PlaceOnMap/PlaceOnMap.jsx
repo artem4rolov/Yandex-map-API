23af7ca807a5afd2a290383ff95f1f8da7189b01
Artem <artem4rolov@gmail.com>
2023-03-29 18:00:50 +0300

    перемещение карты работает

--- src/components/PlaceOnMap/PlaceOnMap.jsx
+++ src/components/PlaceOnMap/PlaceOnMap.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import { YMaps, Map, Placemark } from "@pbe/react-yandex-maps";
 
 import styles from "./PlaceOnMap.module.scss";
@@ -6,15 +6,29 @@ import styles from "./PlaceOnMap.module.scss";
 const PlaceOnMap = ({ coordinates }) => {
   const defaultState = {
     center: [55.751574, 37.573856],
-    zoom: 9,
+    zoom: 12,
     controls: ["zoomControl", "fullscreenControl"],
   };
+
+  const map = useRef();
+
+  const myPanTo = (coordinates) => {
+    if (coordinates) {
+      map.current.panTo(coordinates);
+    }
+  };
+
+  // при получении координат точки - позиционируем карту на эту точку
+  React.useEffect(() => {
+    myPanTo(coordinates);
+  }, [coordinates]);
 
   console.log(coordinates);
 
   return (
     <YMaps>
       <Map
+        instanceRef={map}
         className={styles.mapContainer}
         defaultState={defaultState}
         modules={["control.ZoomControl", "control.FullscreenControl"]}
@@ -27,6 +41,13 @@ const PlaceOnMap = ({ coordinates }) => {
           }}
         />
       </Map>
+      {/* <div className="myMenu">
+        <ul>
+          <li onClick={() => myPanTo([55.612392, 37.029242])}>Пункт 1</li>
+          <li onClick={() => myPanTo([55.46989, 37.947974])}>Пункт 2</li>
+          <li onClick={() => myPanTo([56.108996, 37.501654])}>Пункт 3</li>
+        </ul>
+      </div> */}
     </YMaps>
   );
 };
