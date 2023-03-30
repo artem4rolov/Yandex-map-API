22025cf6e2e89487331f33fd5e17ae0d4e1af136
unknown <artem4rolov@gmail.com>
2023-03-30 14:41:39 +0300

    правки

--- src/components/LocationsList/LocationItem/LocationItem.jsx
+++ src/components/LocationsList/LocationItem/LocationItem.jsx
@@ -1,17 +1,30 @@
 import React from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { setAddress, setPoint } from "../../../redux/slices/locationsSlice";
 
 import styles from "../LocationList.module.scss";
 
-const LocationItem = (item, active) => {
+const LocationItem = (item) => {
   const dispatch = useDispatch();
+  const [active, setActive] = React.useState(null);
+
+  const { currentAddress } = useSelector((state) => state.points);
+
+  // делаем активным текущий выбранный магазин, если в redux store он есть
+  React.useEffect(() => {
+    if (currentAddress) {
+      setActive(currentAddress);
+    }
+  }, [currentAddress]);
+
   // при клике на конкретный магазин в списке - задаем его адрес и координаты в redux store
   const selectPoints = (params, address) => {
     dispatch(setPoint(params));
     dispatch(setAddress(address));
   };
+
   const { address, budgets, latitude, longitude } = item;
+
   return (
     <div
       key={address}
@@ -20,6 +33,7 @@ const LocationItem = (item, active) => {
       }`}
       onClick={() => {
         selectPoints([latitude, longitude], address);
+        setActive(item);
       }}
     >
       <span className={styles.location__adress}>{address}</span>
--- src/components/LocationsList/LocationList.jsx
+++ src/components/LocationsList/LocationList.jsx
@@ -5,11 +5,7 @@ import LocationItem from "./LocationItem/LocationItem";
 import styles from "./LocationList.module.scss";
 
 const LocationList = () => {
-  const [active, setActive] = React.useState(null);
-
-  const { points, currentAddress, filterCoordinates } = useSelector(
-    (state) => state.points
-  );
+  const { points, filterCoordinates } = useSelector((state) => state.points);
 
   // функция, которая проверяет массив кординат на идентичность
   // если координаты города (целые числа) совпадают с координатами магазина (целые числа), фильтруем массив магазинов
@@ -24,13 +20,6 @@ const LocationList = () => {
     return true;
   }
 
-  // делаем активным текущий выбранный магазин, если в redux store он есть
-  React.useEffect(() => {
-    if (currentAddress) {
-      setActive(currentAddress);
-    }
-  }, [currentAddress]);
-
   // рендер будет отличаться, если выбран город
   return (
     <div className={styles.locationList}>
@@ -40,38 +29,21 @@ const LocationList = () => {
           ? points.map((item) => {
               const { address } = item;
 
-              return (
-                <LocationItem
-                  key={address}
-                  active={active}
-                  onClick={() => {
-                    setActive(address);
-                  }}
-                  {...item}
-                />
-              );
+              return <LocationItem key={address} {...item} />;
             })
           : // если выбран город - в списке будут только магазины этого города
             filterCoordinates &&
             points.map((item) => {
               const { address, latitude, longitude } = item;
 
+              // здесь сравниваем совпадения координат магазина и города
               if (
                 arraysEqual(
                   [Math.trunc(latitude), Math.trunc(longitude)],
                   filterCoordinates.map((item) => Math.trunc(item))
                 )
               ) {
-                return (
-                  <LocationItem
-                    key={address}
-                    active={active}
-                    onClick={() => {
-                      setActive(address);
-                    }}
-                    {...item}
-                  />
-                );
+                return <LocationItem key={address} {...item} />;
               }
 
               return null;
